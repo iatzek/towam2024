944e17c0963c4707d5f4255b0c9d145a046bef68
Jacek Kawa <jacek.kawa@polsl.pl>
2024-04-12 10:44:00 +0200

    sprawdz nieujemnosc delty

--- rktowam/Program.cs
+++ rktowam/Program.cs
@@ -9,10 +9,16 @@ namespace rktowam
             double a = 1, b = 0, c = -9;
 
             double delta = b * b - 4 * a * c;
-            double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
-            double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
 
-            Console.WriteLine("pierwiastki to {0} i {1}", x1, x2);
+            if (delta >= 0)
+            {
+
+                double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+                double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+
+                Console.WriteLine("pierwiastki to {0} i {1}", x1, x2);
+            }
+
         }
     }
 }
