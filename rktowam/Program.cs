048e7f521ab4bc6b6f3743dde105de16c5c49851
Jacek Kawa <jacek.kawa@polsl.pl>
2024-04-12 10:42:29 +0200

    pierwsza dzialaca wersja

--- rktowam/Program.cs
+++ rktowam/Program.cs
@@ -6,7 +6,13 @@ namespace rktowam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 1, b = 0, c = -9;
+
+            double delta = b * b - 4 * a * c;
+            double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+            double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+
+            Console.WriteLine("pierwiastki to {0} i {1}", x1, x2);
         }
     }
 }
