22eb6e6270f5d030dd2613d63ad681cc6eeb55a1
Jacek Kawa <jacek.kawa@polsl.pl>
2024-04-12 10:35:04 +0200

    poczatek pracy

---
+++ rktowam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rktowam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
